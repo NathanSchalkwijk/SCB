d4713afa227e8e90960dc29f6787bf728009d3fa
N.s <100487618+NathanSchalkwijk@users.noreply.github.com>
2025-03-09 17:52:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,10 @@ from discord.ext import commands
 import os
 import asyncio
 import time
-from discord import app_commands
 import subprocess
 import sys
 import ast
 import importlib.util
-import pkgutil
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -58,6 +56,24 @@ async def load_commands():
                 print(f"Loaded: {cog_name}")
             except Exception as e:
                 print(f"Failed to load {cog_name}: {e}")
+
+@bot.command(name="reload")
+async def reload(ctx, cog_name: str):
+    try:
+        try:
+            await bot.unload_extension(f"commands.{cog_name}")
+        except Exception:
+            pass
+        filepath = os.path.join("./commands", f"{cog_name}.py")
+        required_modules = get_imports_from_file(filepath)
+        third_party_modules = {mod for mod in required_modules if importlib.util.find_spec(mod) is None}
+        for module in third_party_modules:
+            if not is_package_installed(module):
+                install_package(module)    
+        await bot.load_extension(f"commands.{cog_name}")
+        await ctx.send(f"Cog '{cog_name}' has been reloaded!")
+    except Exception as e:
+        await ctx.send(f"Error reloading cog '{cog_name}': {e}")
 
 @bot.event
 async def on_ready():
